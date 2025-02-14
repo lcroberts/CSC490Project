b908a49729b183f6760e0e1360030d81a3eb8bef
Logan Roberts <logan@lcroberts.dev>
2025-02-14 19:47:01 +0000

    Clear encryption key in a more consistent way.

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,11 +1,7 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
-import { useEffect } from 'react';
 
 export default function GuestLayout({ children }) {
-  useEffect(() => {
-    localStorage.removeItem("studyBuddyEncryptionKey");
-  }, []);
   return (
     <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
       <div>
