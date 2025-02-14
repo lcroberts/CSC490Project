7355444884afe11dbe17ddb25b676192444fd0f4
Logan Roberts <logan@lcroberts.dev>
2025-02-14 20:14:51 +0000

    Move to using php inbuild hash function for Summary caching

--- app/Models/Summary.php
+++ app/Models/Summary.php
@@ -3,22 +3,20 @@
 namespace App\Models;
 
 use App\Helpers\OpenAIHelpers;
-use Illuminate\Database\QueryException;
+use DateInterval;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 use UnexpectedValueException;
 
 class Summary
 {
     public int $id;
+
     public string $content;
 
     public static function generateTextSummary(string $note_content, bool $force_generation = false): string
     {
-        $hash = Hash::make($note_content);
-        if ((!$force_generation) && (Cache::has($hash))) {
+        $hash = hash('sha256', $note_content);
+        if ((! $force_generation) && (Cache::has($hash))) {
             return Cache::get($hash);
         }
 
@@ -29,21 +27,21 @@ class Summary
             Rely only on the provided text, do not include external information. \
             Then, write a list of all vocabulary terms in the text and their definitions, with a prefix that says 'Vocabulary'. \
             The vocabulary list should be formatted as 'word': 'definition'.",
-            "{{{" . $note_content . "}}}"
+            '{{{'.$note_content.'}}}'
         ); // Response is a JSON object
 
         // Decode JSON object to extract only plain text summary
         $json = json_decode($response);
         if (empty($json)) {
-            throw new UnexpectedValueException("Empty JSON object was returned by API while generating summary.");
+            throw new UnexpectedValueException('Empty JSON object was returned by API while generating summary.');
         }
 
         // Extract summary as string
         $summary = $json->choices[0]->message->content;
 
-        Cache::add($hash, $summary);
+        $duration = new DateInterval('P1W');
+        Cache::add($hash, $summary, $duration);
 
         return $summary;
     }
-
 }
