5bb47e5dd3b7de5582d84a0e90bff0451d26c0ce
Logan Roberts <logan@lcroberts.dev>
2025-03-17 17:35:35 +0000

    Update input rule to add new note types based off file extension

--- resources/js/components/markdown-editor/InlineMediaInputRule.js
+++ resources/js/components/markdown-editor/InlineMediaInputRule.js
@@ -1,33 +1,33 @@
 import { InputRule } from "@milkdown/prose/inputrules";
 import { $inputRule } from "@milkdown/utils";
-import { audioNode } from "./CustomNodes";
-
-// const InlineMediaInputRule = $inputRule(
-  // () =>
-  //   // Regex matches links
-  //   new InputRule(/(!?)\[.*?\]\(.*?\)/, (state, match, start, end) => {
-  //     console.log(state);
-  //     console.log(match);
-  //     console.log(start);
-  //     console.log(end);
-  //   }),
-  //
-// );
+import { audioNode, videoNode } from "./CustomNodes";
+import { isAudio, isImage, isVideo } from "@/lib/utils";
 
 const InlineMediaInputRule = $inputRule(
   (ctx) =>
-    new InputRule(
-      /::audio\{src\="(?<src>[^"]+)?"?\}/,
-      (state, match, start, end) => {
-        const [okay, src = ""] = match;
-        const { tr } = state;
-        if (okay) {
-          tr.replaceWith(start - 1, end, audioNode.type(ctx).create({ src }));
-        }
-
+    // Regex matches links
+    new InputRule(/(!?)\[(.*?)\]\((.*?)\)/, (state, match, start, end) => {
+      const { tr } = state;
+      if (match[1] !== "!") {
+        // regular link
         return tr;
-      },
-    ),
+      }
+      const [name, extension] = match[3].split(".");
+      if (!extension) {
+        // no file extension so regular link
+        return tr;
+      }
+
+      if (isImage(extension)) {
+      } else if (isVideo(extension)) {
+        tr.replaceWith(start - 1, end, videoNode.type(ctx).create({ src }));
+      } else if (isAudio(extension)) {
+        tr.replaceWith(start - 1, end, audioNode.type(ctx).create({ src }));
+      } else {
+        // Normal link
+      }
+      return tr;
+    }),
 );
 
 export default InlineMediaInputRule;
