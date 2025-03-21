00bab7720a1a822e7c3e62a40152190b8288ee68
Logan Roberts <logan@lcroberts.dev>
2025-03-21 22:09:28 +0000

    Begin working on uploads

--- resources/js/components/markdown-editor/MarkdownEditor.jsx
+++ resources/js/components/markdown-editor/MarkdownEditor.jsx
@@ -1,20 +1,29 @@
+import MarkdownLogPlugin from './MarkdownLogPlugin';
+import { listener } from '@milkdown/kit/plugin/listener';
 import React from 'react';
 import { Milkdown, MilkdownProvider, useEditor } from '@milkdown/react';
 import "@milkdown/crepe/theme/common/style.css";
 import "@milkdown/crepe/theme/frame.css";
 import { Crepe } from '@milkdown/crepe';
 import DisableAutoEscapeBrackets from './DisableAutoEscapeBrackets';
-import MarkdownLogPlugin from './MarkdownLogPlugin';
 import InlineMediaInputRule from './InlineMediaInputRule';
 import { audioNode, customImageNode, videoNode } from './CustomNodes.js';
-import { listener } from '@milkdown/kit/plugin/listener';
+import { upload, uploadConfig } from '@milkdown/kit/plugin/upload';
+import { customUploader } from './UploadPlugin';
+import { ProsemirrorAdapterProvider, usePluginViewFactory } from '@prosemirror-adapter/react';
+import { html } from "@milkdown/kit/component"
 
 const MilkdownEditor = ({ defaultContent }) => {
-  // `get` returns the editor info
+  const pluginViewFactory = usePluginViewFactory();
   const { get } = useEditor((root) => {
     const crepe = new Crepe({
       root: root,
       defaultValue: defaultContent,
+      featureConfigs: {
+        [Crepe.Feature.BlockEdit]: {
+          slashMenuImageLabel: "Media",
+        }
+      }
     });
     crepe.editor.use([
       DisableAutoEscapeBrackets,
@@ -22,7 +31,15 @@ const MilkdownEditor = ({ defaultContent }) => {
       audioNode,
       videoNode,
       customImageNode,
+      upload,
     ]);
+
+    crepe.editor.config((ctx) => {
+      ctx.update(uploadConfig.key, (prev) => ({
+        ...prev,
+        customUploader,
+      }));
+    });
     // crepe.editor.use([listener, MarkdownLogPlugin]);
     return crepe;
   });
@@ -33,7 +50,9 @@ const MilkdownEditor = ({ defaultContent }) => {
 export const MarkdownEditor = ({ defaultContent }) => {
   return (
     <MilkdownProvider>
-      <MilkdownEditor defaultContent={defaultContent} />
+      <ProsemirrorAdapterProvider>
+        <MilkdownEditor defaultContent={defaultContent} />
+      </ProsemirrorAdapterProvider>
     </MilkdownProvider>
   );
 };
