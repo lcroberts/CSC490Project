9dc068ebee1c319f086aff51f8649e6942b308d5
Logan Roberts <logan@lcroberts.dev>
2025-03-23 19:24:45 +0000

    Get conversion to webp working

--- resources/js/components/markdown-editor/MediaUploadButton.jsx
+++ resources/js/components/markdown-editor/MediaUploadButton.jsx
@@ -1,19 +1,49 @@
+import { isImage, splitToBaseAndExtension } from "@/lib/utils";
 import { useNodeViewContext } from "@prosemirror-adapter/react"
+import { useEffect, useState } from "react";
 
 const MediaUploadButton = () => {
   const nodeViewContext = useNodeViewContext()
-  console.log(nodeViewContext);
-  const onChange = (e) => {
-    const file = e.target.files[0] || undefined;
+  const [finalFile, setFinalFile] = useState(null);
+
+  useEffect(() => {
+    if (!finalFile) {
+      return;
+    }
+    console.log(finalFile);
+  }, [finalFile]);
+
+  const onChange = async (e) => {
+    /** @type {File|null} */
+    let file = e.target.files[0] || undefined;
     if (file) {
-      console.log(file);
+      if (isImage(file.type)) {
+        const imageData  = await createImageBitmap(file);
+        const image = new Image();
+        image.width = imageData.width;
+        image.height = imageData.height;
+        image.src = URL.createObjectURL(file);;
+
+        const canvas = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+        canvas.getContext('2d').drawImage(image, 0, 0);
+        canvas.toBlob((blob) => {
+          const { base, extension } = splitToBaseAndExtension(file.name);
+          // Now we have a `blob` containing webp data
+          // Use the file rename trick to turn it back into a file
+          const myImage = new File([blob], base + '.webp', { type: blob.type });
+          setFinalFile(myImage);
+        }, 'image/webp');
+      }
     }
   }
+
   return (
     <input
       onChange={onChange}
       type="file"
-      className="media-upload my-20 w-full text-slate-500 font-medium text-base bg-gray-100 file:cursor-pointer cursor-pointer file:border-0 file:py-2.5 file:px-4 file:mr-4 file:bg-gray-800 file:hover:bg-gray-700 file:text-white rounded"
+      className="media-upload w-full text-slate-500 font-medium text-base bg-gray-100 file:cursor-pointer cursor-pointer file:border-0 file:py-2.5 file:px-4 file:mr-4 file:bg-gray-800 file:hover:bg-gray-700 file:text-white rounded"
       accept="image/*, video/*, audio/*"
     />
   )
