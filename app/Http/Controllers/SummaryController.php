c8f6e3ee7bcf759140ac307259b055d5eb6d5779
duncanbruce <d_bruce2@uncg.edu>
2025-03-23 18:15:30 -0400

    Add validation and try/catch block to SummaryController.

--- app/Http/Controllers/SummaryController.php
+++ app/Http/Controllers/SummaryController.php
@@ -2,18 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ExceptionHelper;
 use App\Models\Summary;
-use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
+use function Webmozart\Assert\Tests\StaticAnalysis\string;
 
 class SummaryController extends Controller
 {
     public function sendText(Request $request)
     {
-        $noteContent = $request->input('noteContent');
-        $forceGeneration = $request->input('forceGeneration', false);
+        $request->validate([
+            'noteContent' => 'required',
+            'forceGeneration' => 'nullable|boolean',
+        ]);
 
-        $summary = Summary::generateTextSummary($noteContent, $forceGeneration);
+         
+        try {
+            $noteContent = $request->input('noteContent');
+            $forceGeneration = $request->input('forceGeneration', false);
+
+            $summary = Summary::generateTextSummary($noteContent, $forceGeneration);
+        } catch (Exception $err) {
+            return ExceptionHelper::handleException($err);
+        }
 
         return response()->json(['summary' => $summary]);
     }
