db3af2d1738955c2a0f446564c7b881765478528
Logan Roberts <logan@lcroberts.dev>
2025-03-24 01:30:06 +0000

    Get stuff in a spot for testing

--- resources/js/components/markdown-editor/MediaDisplayComponent.jsx
+++ resources/js/components/markdown-editor/MediaDisplayComponent.jsx
@@ -1,25 +1,46 @@
+import useAxios from "@/hooks/useAxios";
 import { isUrl } from "@/lib/utils";
 import { useNodeViewContext } from "@prosemirror-adapter/react"
+import { useEffect, useState } from "react";
 
 const MediaDisplayComponent = () => {
   const { node } = useNodeViewContext();
-  let src;
-  if (isUrl(node.attrs.src)) {
-    src = node.attrs.src;
-  } else {
-    // Fetch media from server in this case
-    src = node.attrs.src;
-  }
+  const http = useAxios();
+  const [src, setSrc] = useState(null);
+
+  useEffect(() => {
+    if (isUrl(node.attrs.src)) {
+      setSrc(node.attrs.src);
+    } else {
+      // Fetch media from server in this case
+      const func = async () => {
+        const noteId = 1;
+        http.get(`/api/media/${noteId}/${node.attrs.src}`).then((res) => {
+          console.log(res);
+        })
+      }
+
+      func();
+    }
+  })
   return (
     <>
-      {node.type.name === "custom-image" &&
-        <img className="custom-image-block" src={src} alt={node.attrs.alt} />
-      }
-      {node.type.name === "audio" &&
-        <audio className="audio-block" src={src} alt={node.attrs.alt} controls />
-      }
-      {node.type.name === "video" &&
-        <video className="video-block" src={src} alt={node.attrs.alt} controls />
+      {src !== null ?
+        <>
+          {node.type.name === "custom-image" &&
+            <img className="custom-image-block" src={src} alt={node.attrs.alt} />
+          }
+          {node.type.name === "audio" &&
+            <audio className="audio-block" src={src} alt={node.attrs.alt} controls />
+          }
+          {node.type.name === "video" &&
+            <video className="video-block" src={src} alt={node.attrs.alt} controls />
+          }
+        </>
+        :
+        <>
+          // Display loading here
+        </>
       }
     </>
   )
