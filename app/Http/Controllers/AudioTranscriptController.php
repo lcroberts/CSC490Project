944834c1632f051a7c51cc9d5180cfd34e2879da
duncanbruce <d_bruce2@uncg.edu>
2025-03-24 15:24:26 -0400

    Adding validate array and try/catch to controller.

--- app/Http/Controllers/AudioTranscriptController.php
+++ app/Http/Controllers/AudioTranscriptController.php
@@ -2,18 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ExceptionHelper;
 use App\Models\AudioTranscript;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Exception;
 
 class AudioTranscriptController extends Controller
 {
     public function sendAudio(Request $request)
     {
-        $audio = $request -> input('audio');
-        $forceGeneration = $request-> input('forceGeneration', false);
+        $request->validate([
+            'audio' => 'required|file',
+            'forceGeneration' => 'nullable|boolean',
+            ]);
 
-        $transcript = AudioTranscript::generateAudioTranscript($audio, $forceGeneration);
+        try{
+            $audio = $request -> file('audio');
+            $forceGeneration = $request-> input('forceGeneration', false);
+
+            $transcript = AudioTranscript::generateAudioTranscript($audio, $forceGeneration);
+        }
+        catch(Exception $err){
+            return ExceptionHelper::handleException($err);
+        }
 
         return response()->json($transcript);
     }
