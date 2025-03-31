3fc3ae462fe4d19c73f93c3c3e97b0f5963e067b
Logan Roberts <logan@lcroberts.dev>
2025-03-31 18:49:55 +0000

    Fix routing issues

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Middleware\AddAuthStatus;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', AddApiToken::class, AddAuthStatus::class])->group(function () {
-    Route::prefix('notes')->group(function () {
+    Route::group(['prefix' => 'notes', 'as' => 'notes.'], function () {
         Route::get('/', [NoteController::class, 'index'])->name('index');
         Route::get('/{note_id}', [NoteController::class, 'get'])->name('get');
         Route::delete('/{note_id}/delete', [NoteController::class, 'delete'])->name('delete');
@@ -17,22 +17,26 @@ Route::middleware(['auth:sanctum', AddApiToken::class, AddAuthStatus::class])->g
         Route::post('/create', [NoteController::class, 'store'])->name('create');
         Route::put('/save', [NoteController::class, 'alter'])->name('save');
     });
-    Route::prefix('media')->group(function () {
+
+    Route::group(['prefix' => 'media', 'as' => 'media.'], function () {
         Route::get('/{note_id}', [NoteController::class, 'indexMedia'])->name('index');
         Route::get('/{note_id}/{name}', [NoteController::class, 'getMedia'])->name('get');
         Route::delete('/{note_id}/{name}/delete', [NoteController::class, 'deleteMedia'])->name('delete');
 
         Route::post('/create', [NoteController::class, 'addMedia'])->name('add');
     });
-    Route::prefix('tags')->group(function () {
+
+    Route::group(['prefix' => 'tags', 'as' => 'tags.'], function () {
         Route::get('/{note_id}', [TagController::class, 'get'])->name('get');
         Route::post('/{note_id}', [TagController::class, 'generate'])->name('create');
         Route::delete('/{tag_id}/delete', [TagController::class, 'delete'])->name('delete');
     });
-    Route::prefix('summary')->group(function () {
+
+    Route::group(['prefix' => 'summary', 'as' => 'summary.'], function () {
         Route::post('/send', [SummaryController::class, 'sendText'])->name('send');
     });
-    Route::prefix('description')->group(function () {
+
+    Route::group(['prefix' => 'description', 'as' => 'description.'], function () {
         Route::post('/send', [ImageDescriptionController::class, 'sendImage'])->name('send');
     });
 });
